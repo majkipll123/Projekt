4f53cc6c899d3895d296735c5d4e0897469cd7d5
majkipll123 <115625416+majkipll123@users.noreply.github.com>
2023-04-07 11:36:20 +0200

    działałoby ale chce zeby wygladalo ladniej

--- z5.py
+++ z5.py
@@ -2,14 +2,16 @@ import os
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-
+from kivy.properties import ObjectProperty, StringProperty
 
 
 
 
 class Kalkulator(Screen):
+    
+    
+    
     pass
-
 
 class Aktywa(Screen):
     pass
@@ -37,14 +39,46 @@ class Windowmanager(ScreenManager):
 class MainApp(MDApp):
     def build(self):#budowanie ekranu
         kv = Builder.load_file("z5.kv")
-
+        
         return kv
+    kurs=4.5
+    waluta2=14
+    def sprawdz(self, text):
+        self.text = text
+        if text == "":
+            self.text = "0"
+        else:
+            self.text = "0"
+    def oblicz(self, waluta, waluta2, kurs):
+        
+        
+        self.waluta = waluta
+        self.waluta2 = float(waluta2)
+        self.kurs = float(kurs)
+
+        waluta = [float(x) for x in waluta]
+        waluta=waluta*kurs
+    def getwaluta(self):
+        return (self.waluta)
+    def getwaluta2(self):
+        return (self.waluta2)
+    def getkurs(self):
+        return (self.kurs)
+    def setwaluta(self, waluta, kurs):
+        self.waluta = waluta
+        self.kurs = kurs
     #def callback1(self):
         #to nie działało ale jest w razie w ale juz dziala w .kv
         #self.root.manager.transition.direction = 'right'
         #self.root.manager.current = 'Ustawienia'
         #print("callback1")
-    
+    def przeliczaj(self, instance): 
+        #tutaj musze pobrac wartosc z pola tekstowego i przeliczyc na walute
+        waluta1=self.root.get_screen('Kalkulator').ids.waluta1.float
+        waluta2=waluta1*2
+        self.root.ids.kalkulator.ids.waluta2.text=waluta2
+        print(waluta1)
+        print(waluta2)
     def czytaj_waluty():
         with open('waluty.txt', 'r') as f:
             lines = f.readlines()
