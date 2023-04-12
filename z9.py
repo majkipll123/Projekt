7bfa8cf330310cdf48e0c23f1f73d77ceb07770d
majkipll123 <115625416+majkipll123@users.noreply.github.com>
2023-04-12 09:58:43 +0200

    jeszcze czytanie z pliku i git

--- z9.py
+++ z9.py
@@ -1,90 +1,93 @@
-import kivy
-
-kivy.require("1.10.1")
 from kivy.uix.scrollview import ScrollView
-import os
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.label import MDLabel
-
+from kivymd.app import MDApp
+from kivymd.uix.label import  MDLabel
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty, StringProperty
 
 
 class Przelicznik():
-    def __init__(self, name, balance,kurs):
+    def __init__(self, name, balance, kurs):
         self.kurs = kurs
         self.name = name
-        self.__balance__ = balance
+        self._balance = balance
 
     def getBalance(self):
-        return (self.__balance__)
+        return self._balance
 
     def setBalance(self, zmena):
-        self.__balance__ = str(zmena * self.kurs) + " PLN"
+        self._balance = str(zmena * self.kurs) + " PLN"
 
-"""
-class FloatInput(acc):
-    pat = re.compile('[^0-9.]')
-    def insert_text(self, substring, from_undo=False):
-        pat = self.pat
-        if '.' in self.text:
-            # don't allow more than one decimal point
-            s = re.sub(pat, '', substring)
-        else:
-            s = '.'.join([
-                re.sub(pat, '', ss)
-                for ss in substring.split('.', 1)
-            ])
-        return super(FloatInput, self).insert_text(s, from_undo=from_undo)
-"""
+
 
 class Kalkulator(Screen):
     acc = ObjectProperty(None)
     balance = StringProperty('')
-    
+
     def __init__(self, **kwargs):
         super(Kalkulator, self).__init__(**kwargs)
         self.acc = Przelicznik("Main", 0, 4.3)
         self.update_balance()
-    # Set label positions
+        # Set label positions
         self.ids.lb1.pos_hint = {'center_x': 1, 'center_y': 0.8}
-    
         self.ids.lb2.pos_hint = {'center_x': 1, 'center_y': 0.6}
-
         self.ids.lb3.pos_hint = {'center_x': 1, 'center_y': 0.5}
-
         self.ids.tp.pos_hint = {'center_x': 0.5, 'center_y': 0.35}
-
         self.ids.btn1.pos_hint = {'center_x': 0.5, 'center_y': 0.2}
-
 
     def update_balance(self):
         self.balance = str(self.acc.getBalance())
 
+
 class Custom(Screen):
+    acc = ObjectProperty(None)
+    balance = StringProperty('')
+
+    def __init__(self, **kwargs):
+        super(Custom, self).__init__(**kwargs)
+        self.acc = Przelicznik("Main", 0, 4.3)
+        self.update_balance()
+        # Set label positions
+        self.ids.lb1.pos_hint = {'center_x': 1, 'center_y': 0.8}
+        self.ids.lb2.pos_hint = {'center_x': 1, 'center_y': 0.6}
+        self.ids.lb3.pos_hint = {'center_x': 1, 'center_y': 0.5}
+        self.ids.tp.pos_hint = {'center_x': 0.5, 'center_y': 0.35}
+        self.ids.btn1.pos_hint = {'center_x': 0.5, 'center_y': 0.2}
+
+    def update_balance(self):
+        self.balance = str(self.acc.getBalance())
+
+
+class Aktywa(Screen):
     pass
 
-class Aktywa(ScrollView):
-    pass
+
+
 
 class Ustawienia(Screen):
     pass
 
+
 Builder.load_file("z8.kv")
+
 
 class MainApp(MDApp):
     def build(self):
+
         sm = ScreenManager()
+
         sm.add_widget(Kalkulator(name='Kalkulator'))
-        sm.add_widget(Custom(name='Custom')) # use Custom here
+
+        sm.add_widget(Custom(name='Custom'))
+        
         sm.add_widget(Aktywa(name='Aktywa'))
+        
         sm.add_widget(Ustawienia(name='Ustawienia'))
+
         return sm
-
-
-
 
 
 if __name__ == "__main__":
